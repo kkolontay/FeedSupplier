5521af6f2cdc7528c9a150e93b7b4ca9c2cb0668
Konstantin Kolontay <kkolontay@gmail.com>
2021-06-01 19:17:03 -0400

    Initial Commit

---
+++ FeedSupplier/FeedSupplier.h
@@ -0,0 +1,18 @@
+//
+//  FeedSupplier.h
+//  FeedSupplier
+//
+//  Created by kkolontay on 6/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedSupplier.
+FOUNDATION_EXPORT double FeedSupplierVersionNumber;
+
+//! Project version string for FeedSupplier.
+FOUNDATION_EXPORT const unsigned char FeedSupplierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedSupplier/PublicHeader.h>
+
+
